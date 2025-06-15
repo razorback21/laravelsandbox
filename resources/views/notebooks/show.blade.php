d4d2ef48065f33fd7645c1927a10fb0ff971e879
Donz <donnygapulao@gmail.com>
2025-06-15 19:37:37 +0800

    added alert success

--- resources/views/notebooks/show.blade.php
+++ resources/views/notebooks/show.blade.php
@@ -6,11 +6,7 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            @if(session('success'))
-            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
-                {{ session('success') }}
-            </div>
-            @endif
+            <x-alert-success />
             <div class="flex">
                 <x-link-button href="{{ route('notebooks.edit', $notebook) }}" class="ml-auto">Edit</x-link-button>
                 <form method="post">
