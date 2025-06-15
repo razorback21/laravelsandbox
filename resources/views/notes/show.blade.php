5f358851f854ce9cc8af99aa4b9b8cb2b70a2dc9
Donz <donnygapulao@gmail.com>
2025-06-15 14:15:09 +0800

    revrt back to old code

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -6,10 +6,11 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <p class="mt-4 whitespace-pre-wrap">Updated at: {{ $note->updated_at->diffForHumans() }}</p>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10">
-                <h1 class="text-bold">{{ $note->name }}</h1>
-                <p class="mt-3">{{ $note->text }}</p>
-                <p class="mt-4 whitespace-pre-wrap">Updated at: {{ $note->updated_at->diffForHumans() }}</p>
+                <h1 class="text-bold text-4xl">{{ $note->title }}</h1>
+                <p class="mt-4">{{ $note->text }}</p>
+                
                 <p class="mt-3">
                     <a href="{{ route('notes.edit', $note) }}" class="text-blue-500 hover:underline">Edit</a>
                 </p>
